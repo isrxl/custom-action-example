0de1aabebf7b726b0ac8431f9c78c314b8228fdf
Israel Orenuga <20260651+isrxl@users.noreply.github.com>
2024-09-05 00:39:07 +1000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# # Get the input value
-# MY_INPUT=$1
-
-# # Print the input value to the console
-# echo "Input value: $MY_INPUT"
-
-# # Perform some logic, e.g., fetch data using curl
-# DATA=$(curl -s https://api.github.com)
-
-# # Use jq to parse the data
-# REPO_COUNT=$(echo "$DATA" | jq '.public_repos')
-
-# # Output a custom message
-# echo "GitHub has $REPO_COUNT public repositories"
-
-# # Set an output variable (if needed)
-# echo "::set-output name=my-output::$REPO_COUNT"
-
-
-### Iteration - 2
-
 # Get the input value 
 MY_INPUT=$1
 
@@ -37,5 +16,6 @@ REPO_COUNT=$(echo "$DATA" | jq '.public_repos')
 echo "Microsoft Azure organization has $REPO_COUNT public repositories"
 
 # Set an output variable (if needed)
-echo "::set-output name=my-output::$REPO_COUNT"
+#echo "::set-output name=my-output::$REPO_COUNT"
+echo "my-output=$REPO_COUNT" >> "$GITHUB_ENV"
 
